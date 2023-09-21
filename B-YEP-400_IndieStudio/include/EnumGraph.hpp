7549cd57ded4bcc227a79d934a9cdcc38173d578
Chloé Chauvin <60068251+Nekory23@users.noreply.github.com>
2023-09-21 20:28:13 +0200

    Add files via upload

---
+++ B-YEP-400_IndieStudio/include/EnumGraph.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2021
+** IndieStudio
+** File description:
+** enumGraph
+*/
+
+#ifndef ENUMGRAPH_HPP_
+#define ENUMGRAPH_HPP_
+
+namespace graph {
+    enum stateGraph {
+        CREDIT,
+        MENU,
+        GAME,
+        PAUSE,
+        OVER
+    };
+
+    enum returnMenu {
+        START = 0,
+        LOAD = 0
+    };
+}
+
+#endif /* !ENUMGRAPH_HPP_ */
